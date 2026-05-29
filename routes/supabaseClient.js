a949e0400b65a5a26923b895228037c2dd869b4e
Keké <kemmily.carvalho@aluno.senai.br>
2026-05-29 15:51:08 -0300

    Add files via upload

--- routes/supabaseClient.js
+++ routes/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://xiaukzzqjixkycuepqzm.supabase.co'
-const supabaseAnonKey = 'sb_publishable_ANUwxvL9ux63ATKGyX1leA_MIhWIUOg'
+const supabaseUrl = 'https://qxbhoxcqozyfwscylmvt.supabase.co'
+const supabaseAnonKey = 'sb_publishable_ZCUu5Xq3rvMWQQ3U3dFq-A_pC0UARHV'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
